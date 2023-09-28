4c6ea03d0ad6ca4e5bd3a710c8ab18e84a23db3c
brinleylott <bvg56010@gmail.com>
2023-09-28 12:32:39 -0600

    BOM

--- lesson2/scripts/bom.js
+++ lesson2/scripts/bom.js
@@ -1,17 +1,30 @@
-const input = document.querySelector('#favchap');
-const button = document.querySelector('button');
-const ul = document.querySelector('#list');
-const feedback = document.querySelector('#feedback');
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+const feedback = document.querySelector("#feedback");
+const li = document.createElement("li");
+const deleteButton = document.createElement("button");
 
-button.addEventListener('click', () => {
-
-    if(input.value) {
-       console.log('clicked');
-       feedback.textContent=' '
-    }
-    else {
-        input.focus();
-        //give feedback to user//
-        feedback.textContent = 'Please enter a Book and Chapter'
-    }
+button.addEventListener("click", () => {
+  if (input.value) {
+    console.log("clicked");
+    feedback.textContent = " ";
+  } else {
+    input.focus();
+    //give feedback to user//
+    feedback.textContent = "Please enter a Book and Chapter";
+  }
+  li.textContent = input.value;
+  deleteButton.textContent = "❌";
+  li.append(deleteButton);
+  list.append(li);
 });
+
+deleteButton.addEventListener("click", function () {
+  list.removeChild(li);
+  input.focus();
+});
+
+input.focus();
+input.value = "";
+
