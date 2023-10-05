f33432a28e7c8672f2c04195a14381553d222445
brinleylott <bvg56010@gmail.com>
2023-10-05 09:56:13 -0600

    dark mode + chamber home

--- scripts/advnaced.js
+++ scripts/advnaced.js
@@ -6,3 +6,10 @@ hamButton.addEventListener('click', () => {
 	hamButton.classList.toggle('open');
 });
 
+const modeButton = document.querySelector('#mode')
+const main = document.querySelector("main")
+
+modeButton.addEventListener('click', () => {
+	modeButton.classList.toggle('darkMode');
+	main.classList.toggle('darkMode');
+});
