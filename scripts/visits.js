7caeb7e42d2d18f1ad29995712a95b6b4d3a4d52
brinleylott <bvg56010@gmail.com>
2023-10-19 15:07:42 -0600

    updating index.html and adding discover page.

---
+++ scripts/visits.js
@@ -0,0 +1,14 @@
+let numVisits = 1;
+
+if (localStorage.getItem('numVisits')) {
+    numVisits = parseInt(localStorage.getItem('numVisits'));
+    numVisits++;
+}
+
+// set local storage with number of visits
+localStorage.setItem('numVisits', numVisits);
+
+// display number of visits
+let numVisitsSpan = document.querySelector('#number-of-visits');
+numVisitsSpan.textContent = numVisits;
+
