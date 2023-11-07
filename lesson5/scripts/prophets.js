443bd6a083fd04fce5494b2ee78679fc8bb6caf3
brinleylott <bvg56010@gmail.com>
2023-11-07 10:42:28 -0700

    changes

---
+++ lesson5/scripts/prophets.js
@@ -0,0 +1,22 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json'
+const cards = document.querySelector('cards')
+
+async function getProphetsData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayProphets(data.prophets)
+    console.log(data);
+}
+
+function displayProphets(prophets) {
+    prophets.forEach(prophet => {
+        const card = document.createElement('section')
+        const fullName = document.createElement('h2');
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        card.appendChild(fullName);
+        cards.appendChild(card);
+    });
+}
+
+getProphetsData();
