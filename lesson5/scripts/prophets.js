4f2e9db38ea0981501adde4eff7b59e916596c6c
brinleylott <bvg56010@gmail.com>
2023-11-11 19:09:51 -0700

    yuh

--- lesson5/scripts/prophets.js
+++ lesson5/scripts/prophets.js
@@ -1,20 +1,35 @@
-const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json'
-const cards = document.querySelector('#cards')
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
 
 async function getProphetsData() {
-    const response = await fetch(url);
-    const data = await response.json();
-    // displayProphets(data.prophets)
-    console.table(data.prophets);
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        // console.table(data.prophets);
+        displayProphets(data.prophets);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
 }
 
 function displayProphets(prophets) {
     prophets.forEach(prophet => {
-        const card = document.createElement('section')
+        const card = document.createElement('section');
         const fullName = document.createElement('h2');
+        let portrait = document.createElement('img');
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
 
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', 'Prophet portrait');
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '330');
+        portrait.setAttribute('height', '440');
+
+        // Log the image URL to the console
+        // console.log('Image URL:', prophet.imageurl);
+
         card.appendChild(fullName);
+        card.appendChild(portrait);
         cards.appendChild(card);
     });
 }
