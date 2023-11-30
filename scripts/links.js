52b4b458d2be1d717e6d97917019d2a5a80be3c9
brinleylott <bvg56010@gmail.com>
2023-11-30 11:44:42 -0700

    json

--- scripts/links.js
+++ scripts/links.js
@@ -1,29 +1,51 @@
 const baseURL = "https://brinleylott.github.io/wdd230/";
 const linksURL = "https://brinleylott.github.io/wdd230/data/links.json";
-
+const myLinks = document.querySelector('#links-container')
 // ... Your fetch and getLinks function ...
 
-function displayLinks(data) {
-    const linksArray = data.links;
-  
-    const linksContainer = document.getElementById('links-container');
-    
-    linksArray.forEach(link => {
-      const linkElement = document.createElement('a');
-      linkElement.href = link.url;
-      linkElement.textContent = link.title;
-  
-      linksContainer.appendChild(linkElement); // Append each link to the container
-    });
+async function getLinks() {
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  displayLinks(data.lessons);
+}
+function displayLinks(lessons) {
+    lessons.forEach((lesson) => {
+      const lesson = document.createElement('p');
+      lesson.textContent = `${lesson.lesson}`;
+      const lessonLinks = lesson.links;
+
+      lessonLinks.forEach((lessonLink, index)=> {
+        const link = document.createElement('a');
+        link.textContent = `${lessonLink.title}`;
+        link.setAttribute('href', lessonLink.url);
+        lesson.appendChild(link);
+
+        if(index !== lessonLinks.length -1){
+          const spacer = documnet.createElement('span');
+          spacer.textContent = ' | ';
+          lesson.appendChild(spacer);
+        }
+      })
+      myLinks.appendChild(lesson)
+    })
   }
   
   // ... Continue with the rest of your code ...
   
   
-async function getLinks() {
-  const response = await fetch(linksURL);
-  const data = await response.json();
-  displayLinks(data);
-}
+
   
   getLinks();
+
+
+  // const linksArray = data.links;
+  
+    // const linksContainer = document.getElementById('links-container');
+    
+    // linksArray.forEach(link => {
+    //   const linkElement = document.createElement('a');
+    //   linkElement.href = link.url;
+    //   linkElement.textContent = link.title;
+  
+    //   linksContainer.appendChild(linkElement); // Append each link to the container
+    // });
