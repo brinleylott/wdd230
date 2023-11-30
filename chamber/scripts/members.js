4f5099cd345a233d5d4c476d487b95a22a3c4ccb
brinleylott <bvg56010@gmail.com>
2023-11-30 09:38:07 -0700

    json update

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -4,35 +4,24 @@ const cards = document.querySelector('#cards');
 async function getMembers() {
     try {
         const response = await fetch(url);
-        const result = await response.json();
+        const data = await response.json();
+        displayMembers(data.members)
 
-        if (result && result.length > 0) {
-            console.log(result[0].name);
-            return result;
-        } else {
-            console.error('No data found in the API response.');
-            return []; // Return an empty array or handle the case when there's no data
-        }
     } catch (error) {
         console.error('Error fetching data:', error);
         return []; // Return an empty array or handle the error
     }
 }
 
-// "name": "IDWEB",
-//     "address": {
-//       "street1": "2115 S Vista Ave",
-//       "street2": "",
-//       "city": "boise",
-//       "state": "Idaho",
-//       "zip": "83705"
-//     },
-//     "phone": "209-324-8765",
-//     "url": "https://www.idweb.it/en/",
-//     "imageUrl": "https://www.idweb.it/en/wp-content/uploads/sites/4/2020/01/logo-IdSurvey-cati-cawi-capi-research-software.png",
-//     "membershipLevel": "Gold"
-
 function displayMembers(members) {
+    const cardsContainer = document.querySelector('#cards');
+    
+    if (members.length === 0) {
+        // Handle the case when there's no data
+        cardsContainer.innerHTML = '<p>No members found.</p>';
+        return;
+    }
+
     members.forEach(member => {
         const card = document.createElement('section');
        
@@ -41,22 +30,33 @@ function displayMembers(members) {
         const address = document.createElement('p');
         const phone = document.createElement('p');
         const membershipLevel = document.createElement('p');
+        
         fullName.textContent = `${member.name}`;
-        address.textContent = `${member.address}`;
-        phone.textContent = `${member.phone}`;
-        membershipLevel.textContent = `${member.membershipLevel}`;
+        
+        // Extract relevant details from the address
+        const { street1, street2, city, state, zip } = member.address;
+        address.textContent = `${street1}, ${street2 ? street2 + ', ' : ''}${city}, ${state} ${zip}`;
+        
+        phone.textContent = `Phone: ${member.phone}`;
+        membershipLevel.textContent = `Membership Level: ${member.membershipLevel}`;
 
         image.setAttribute('src', member.imageUrl);
         image.setAttribute('alt', 'company logo');
         image.setAttribute('loading', 'lazy');
-        image.setAttribute('width', '230');
-        image.setAttribute('height', '240');
-
-        card.appendChild(fullName); // Append to the card container
+        image.setAttribute('width', '120');
+        image.setAttribute('height', '70');
+        
         card.appendChild(image);
-        cards.appendChild(card);
-    })
+        card.appendChild(fullName);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(membershipLevel);
+        
+
+        cardsContainer.appendChild(card);
+    });
 }
 
 // Call the functions
 getMembers().then(members => displayMembers(members));
+
