0570735dc805d00991a1f6fdd24ac3c04cecb08a
brinleylott <bvg56010@gmail.com>
2023-12-07 16:50:04 -0700

    weather forecast

--- chamber/scripts/events.js
+++ chamber/scripts/events.js
@@ -1,18 +1,48 @@
+// var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length} ;
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   x[slideIndex-1].style.display = "block";
+// }
+
 var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+    showDivs(slideIndex += n);
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var descriptions = document.getElementsByClassName("slide-description");
+
+    if (n > x.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = x.length;
+    }
+
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < descriptions.length; i++) {
+        descriptions[i].style.display = "none";
+    }
+
+    x[slideIndex - 1].style.display = "block";
+    descriptions[slideIndex - 1].style.display = "block";
 }
 
--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -2,7 +2,16 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=43.82&lon=111.79&appid=4f95f54f9f3274981432648406c80731&units=imperial'
+
+
+const date = document.querySelector('#dateTime');
+const high = document.querySelector('#high');
+const low = document.querySelector('#low')
+const icon = document.querySelector('#forecastIcon');
+const weatherDesc = document.querySelector('figcaption')
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=43.82&lon=111.79&appid=4f95f54f9f3274981432648406c80731&units=imperial';
+const forecasturl = 'https://api.openweathermap.org/data/2.5/forecast?lat=43.82&lon=111.79&appid=4f95f54f9f3274981432648406c80731&units=imperial';
 
 async function apiFetch() {
   try {
@@ -29,3 +38,116 @@ function displayResults(data) {
   weatherIcon.setAttribute('alt', desc);
   captionDesc.textContent = `${desc}`;
 }
+
+
+function showForecast(forecastList) {
+  const forecastContainer = document.querySelector('#forecast');
+
+  // Get the current date
+  const currentDate = new Date();
+
+  // Declare uniqueEntries object to store unique entries for each day
+  const uniqueEntries = {};
+
+  // Filter forecast entries for the next 3 days
+  const next3DaysForecast = forecastList.filter(entry => {
+    const entryDate = new Date(entry.dt_txt);
+    const timeDifference = entryDate.getTime() - currentDate.getTime();
+    const daysDifference = timeDifference / (1000 * 3600 * 24);
+
+    // Include entries within the next 3 days
+    return daysDifference >= 0 && daysDifference < 3;
+  });
+
+//   // Assuming you want to display all the forecast entries for the next 3 days
+//   next3DaysForecast.forEach((forecast, index) => {
+//     const forecastDate = new Date(forecast.dt_txt).toLocaleDateString();
+
+    
+//     // Check if an entry for this day has already been added
+//     if (!uniqueEntries[forecastDate]) {
+//       const forecastTempMax = forecast.main.temp_max;
+//       const forecastTempMin = forecast.main.temp_min;
+//       const srcIcon = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+//       const description = forecast.weather[0].description;
+
+//       // Create forecast elements
+//       const forecastItem = document.createElement('div');
+//       forecastItem.classList.add('forecast-item');
+//       forecastItem.innerHTML = `
+//         <p class="forecast-date">${forecastDate}</p>
+//         <img class="forecast-icon" src="${srcIcon}" alt="${description}">
+//         <p class="forecast-desc">${description}</p>
+//         <p class="forecast-tempH">${forecastTempMax}&deg;F - ${forecastTempMin}&deg;F</p>       
+        
+//       `;
+
+//       // Append forecast item to the container
+//       forecastContainer.appendChild(forecastItem);
+      
+//       // Mark this day as processed
+//       uniqueEntries[forecastDate] = true;
+//     }
+//   });
+// }
+// ...
+
+// ...
+
+next3DaysForecast.forEach((forecast, index) => {
+  const forecastDate = new Date(forecast.dt_txt);
+
+  // Format the date as "Friday December 8"
+  const formattedDate = forecastDate.toLocaleDateString('en-US', {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  });
+
+  // Split the formatted date into two lines
+  const [weekday, dayAndMonth] = formattedDate.split(' ');
+
+  // Check if an entry for this day has already been added
+  if (!uniqueEntries[formattedDate]) {
+    const forecastTempMax = forecast.main.temp_max;
+    const forecastTempMin = forecast.main.temp_min;
+    const srcIcon = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+    const description = forecast.weather[0].description;
+
+    // Create forecast elements
+    const forecastItem = document.createElement('div');
+    forecastItem.classList.add('forecast-item');
+    forecastItem.innerHTML = `
+      <p class="forecast-date">${weekday}<br>${dayAndMonth}</p>
+      <img class="forecast-icon" src="${srcIcon}" alt="${description}">
+      <p class="forecast-desc">${description}</p>
+      <p class="forecast-tempH">${forecastTempMax}&deg;F - ${forecastTempMin}&deg;F</p>
+    `;
+
+    // Append forecast item to the container
+    forecastContainer.appendChild(forecastItem);
+
+    // Mark this day as processed
+    uniqueEntries[formattedDate] = true;
+  }
+});
+
+}
+
+async function forecast() {
+  const forecastContainer = document.querySelector('#forecast');
+
+  try {
+    const response = await fetch(forecasturl);
+    if (response.ok) {
+      const data2 = await response.json();
+      showForecast(data2.list);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+forecast();
